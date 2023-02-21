b7d0679956f9803637ebf2d4044b157f0d78b87f
Newton Ombese <newtonombese1@gmail.com>
2023-02-21 08:31:28 +0300

    :zap::art:feat: Worked on the filtering of data

--- src/components/MakeupPage/Makeup.js
+++ src/components/MakeupPage/Makeup.js
@@ -1,94 +1,73 @@
-import { useEffect, useState } from "react"
+import React, { useState, useEffect } from "react";
 import Navbar from "../Navbar/Navbar"
 import './Makeup.css'
 
-const Makeup = () => {
-    const [makeup, setMakeup] = useState([])
+function App() {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [productType, setProductType] = useState("");
 
-    useEffect(()=>{
-        fetch('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
-        .then((response) => response.json())
-        .then((data) =>{
-            console.log(data)
-            setMakeup(data)
-        })
-    })
+  useEffect(() => {
+    fetch("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+        setFilteredProducts(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
+  const handleFilterChange = (event) => {
+    setProductType(event.target.value);
+    const filtered = products.filter((product) => product.product_type === event.target.value);
+    setFilteredProducts(filtered);
+  };
 
-   //  const [category, setCategory] =useState('')
-
-const filterResult = (categoryItem) =>{
-const updatedItem = product_type.filter((currentData)=>{
-   return currentData.product_type === categoryItem;
-});
-setMakeup(updatedItem)
-}
-
-
-
-    return (  
-<>
-<Navbar />
-{/* <h1 className="text-center">Maybelline Collections</h1> */}
-<div className="container-fluid mx-2">
-    <div className="row mt-5 mx-2">
-        <div className="col-md-3">
-            <button className="btn btn-warning w-100 mb-4" onClick={()=>filterResult('All')}>All</button>           
-            <button className="btn btn-warning w-100 mb-4" onClick={()=>filterResult('Foundation')}>Foundation</button>
-            <button className="btn btn-warning w-100 mb-4" onClick={()=>filterResult('Lipstick')}>Lipstick</button>
-            <button className="btn btn-warning w-100 mb-4" onClick={()=>filterResult('Lipstick')}>Bronzer</button>
-            <button className="btn btn-warning w-100 mb-4" onClick={()=>filterResult('Lipstick')}>Blush</button>
-            <button className="btn btn-warning w-100 mb-4" onClick={()=>filterResult('Lipstick')}>Eyeshadow</button>
-            <button className="btn btn-warning w-100 mb-4" onClick={()=>filterResult('Lipstick')}>Eyeliner</button>
-            <button className="btn btn-warning w-100 mb-4" onClick={()=>filterResult('Lipstick')}>Nail Polish</button>
-            <button className="btn btn-warning w-100 mb-4" onClick={()=>filterResult('Lipstick')}>Mascara</button>
-        </div>
-        <div className="col-md-9">
-        <section>
-   
-   {/* <div class="heading">
-       <h1>Maybelline Products</h1>
-     
-   </div> */}
-   
-   <div class="container">
-    
-       {makeup.map((product) => {
-         const {id, name, product_link, product_type, price, image_link} = product
-          return (
-           
-    <div class="box" key={id}>
-     
-     <a href={product_link}>
-      <img src={image_link} />
-
-     </a>
-       <h2>{name}</h2>
-       <h2>{product_type}</h2>
-       <span>${price}</span>
-      
-       <div class="options">
-           <a href="#">Favorite</a>
-           <a href="#">Delete</a>
-       </div>
-   </div>
-
-     
-          );
-       })}
- </div>
-</section>  
-        </div>
+  return (
+    <div>
+        <Navbar />
+        <h1>Maybelline Products</h1>
+        <label htmlFor="productTypeFilter">Filter by product type:</label>
+        <select id="productTypeFilter" value={productType} onChange={handleFilterChange}>
+            <option value="">All</option>
+            <option value="blush">Blush</option>
+            <option value="bronzer">Bronzer</option>
+            <option value="eyebrow">Eyebrow</option>
+            <option value="eyeliner">Eyeliner</option>
+            <option value="eyeshadow">Eyeshadow</option>
+            <option value="foundation">Foundation</option>
+            <option value="lipstick">Lipstick</option>
+            <option value="mascara">Mascara</option>
+        </select>
+        <section>   
+            <div class="container">
+                {filteredProducts.map((product) => {
+                const {id, name, product_link, product_type, price, image_link} = product
+                return (       
+                <div class="box" key={product.id}>
+                    <a href={product_link}>
+                    <img src={image_link} />
+                    </a>
+                    <h2>{product.name}</h2>
+                    <h2>{product_type}</h2>
+                    <span>${price}</span>
+                
+                <div class="options">
+                    <a href="#">Favorite</a>
+                    <a href="#">Delete</a>
+                </div>
+                </div>
+                )})}
+            </div>
+        </section>  
     </div>
-</div>
-
-
-
-</>
-
-
-
-    );
+  );
 }
- 
-export default Makeup;
+
+export default App;
+
+
+
+
