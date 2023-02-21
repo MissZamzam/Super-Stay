12a3514416e34b6ede5c755c5543a17926314f23
Zamzam <Zamzamhassan44@gmail.com>
2023-02-21 10:07:32 +0300

    saved change

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,8 +5,8 @@ const Navbar = () => {
   <div class="container-fluid">
     <a class="navbar-brand navbar mb-4">Maybelline</a>
     <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-      <button class="btn btn-outline-success" type="submit">Search</button>
+      {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" /> */}
+      {/* <button class="btn btn-outline-success" type="submit">Search</button> */}
     </form>
   </div>
 </nav>
