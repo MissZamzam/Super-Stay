351271eb241a554cdab627702913588d307ef294
Zamzam <Zamzamhassan44@gmail.com>
2023-06-25 23:41:48 +0300

    cards styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,37 +1,32 @@
-import './Navbar.css'
-import { Link } from 'react-router-dom';
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  return (
+    <nav>
+      <input id="nav-toggle" type="checkbox" />
+      <div class="logo">
+        SuperStay<strong></strong>
+      </div>
+      <ul class="links">
+        <Link to="/">
+          <li>
+            <a href="#home">Products</a>
+          </li>
+        </Link>
+        <Link to="/favorite">
+          <li>
+            <a href="#projects">Favorites</a>
+          </li>
+        </Link>
+      </ul>
+      <label for="nav-toggle" class="icon-burger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </label>
+    </nav>
+  );
+};
 
-
-
-
-
-
-  return ( 
-<nav>
-	<input id="nav-toggle" type="checkbox" />
-	<div class="logo">SuperStay<strong></strong></div>
-	<ul class="links">
-
-
-    <Link to='/'>
-    		<li><a href="#home">Products</a></li>
-    </Link>
-    <Link to='/favorite'>
-    		<li><a href="#projects">Favorites</a></li>
-    </Link>
-
-	</ul>
-	<label for="nav-toggle" class="icon-burger">
-		<div class="line"></div>
-		<div class="line"></div>
-		<div class="line"></div>
-	</label>
-</nav>
-
-
- );
-}
- 
 export default Navbar;
